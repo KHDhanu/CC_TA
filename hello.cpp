d5d47f66a5dece54435126988d1357109b820b1d
KHDhanu <khdhanu5243@gmail.com>
2025-03-21 05:46:31 +0000

    Testing webhook automation

--- hello.cpp
+++ hello.cpp
@@ -8,5 +8,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "Jenkins Webhook is now fully working!\n";
   return 0;
 }
